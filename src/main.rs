f4f586c2f550b2a756703cf9ce1ae080fb448707
yxqsnz <yxqsnz@gmail.com>
2021-12-26 14:06:48 -0300

    +999 social credits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod config;
 use config::bot_config;
-fn main() {
+use serenity::Client;
+#[tokio::main]
+async fn main() {
     let cfg = bot_config::Config::new();
-    println!("{:?}", cfg);
+
+     let mut client = Client::builder(cfg.bot.token)
+        //.event_handler(Handler)
+        .application_id(cfg.bot.bot_id)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        eprintln!("Serenity -9999 social credits: {:?}", why);
+	}
 }
